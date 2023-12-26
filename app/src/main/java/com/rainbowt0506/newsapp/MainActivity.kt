ede096cb3be88f4d47bf5d137e151c50fe3499e4
RainBowT <rainbowt0506@gmail.com>
2023-12-26 17:40:14 +0800

    feat: Add splash screen configuration

---
+++ app/src/main/java/com/rainbowt0506/newsapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.rainbowt0506.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.rainbowt0506.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            NewsAppTheme {
+
+            }
+        }
+    }
+}
