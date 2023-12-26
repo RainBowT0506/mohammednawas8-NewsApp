93994997c74260f08b9fee9d2a880d565a5f2a80
RainBowT <rainbowt0506@gmail.com>
2023-12-26 17:37:24 +0800

    initial commit

---
+++ app/src/main/java/com/rainbowt0506/newsapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.rainbowt0506.newsapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF1C1E21)
+val Blue = Color(0xFF1877F2)
+
+val DarkRed = Color(0xFFC30052)
+val LightRed = Color(0xFFFF84B7)
+
+val LightBlack = Color(0xFF3A3B3C)
