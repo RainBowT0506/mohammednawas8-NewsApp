1941dbc30f98577ba556fbf00d72308e0fe7e3d7
RainBowT <rainbowt0506@gmail.com>
2023-12-27 10:36:10 +0800

    feat: Add OnBoardingScreen composable

---
+++ app/src/main/java/com/rainbowt0506/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -0,0 +1,36 @@
+package com.rainbowt0506.newsapp.presentation.onboarding
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.rainbowt0506.newsapp.presentation.onboarding.components.OnBoardingPage
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun OnBoardingScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        val pageState = rememberPagerState(initialPage = 0)
+
+        val buttonState = remember {
+            derivedStateOf {
+                when (pageState.currentPage) {
+                    0 -> listOf("", "Next")
+                    1 -> listOf("Back", "Next")
+                    2 -> listOf("Back", "Get Started")
+                    else -> listOf("", "")
+                }
+            }
+        }
+
+        HorizontalPager(pageCount = pages.size, state = pageState) { index ->
+            OnBoardingPage(page = pages[index])
+        }
+    }
+}
