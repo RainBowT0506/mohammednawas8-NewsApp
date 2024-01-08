fa08d76ebceb8245027689ef48607d16b1d7ad28
RainBowT <rainbowt0506@gmail.com>
2024-01-08 11:31:50 +0800

    feat: Implement Onboarding Screen
    - Introduce the OnboardingScreen with page indicators, navigation buttons, and interaction logic.
    - Implement a spacer to adjust layout weights, ensuring proper spacing within the screen.
    - Apply WindowCompat.setDecorFitsSystemWindows(window, false) in MainActivity to customize the behavior of system window insets.

--- app/src/main/java/com/rainbowt0506/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/rainbowt0506/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -1,15 +1,30 @@
 package com.rainbowt0506.newsapp.presentation.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.rainbowt0506.newsapp.presentation.Dimens.MediumPadding2
+import com.rainbowt0506.newsapp.presentation.Dimens.PageIndicatorWidth
+import com.rainbowt0506.newsapp.presentation.common.NewsButton
+import com.rainbowt0506.newsapp.presentation.common.NewsTextButton
 import com.rainbowt0506.newsapp.presentation.onboarding.components.OnBoardingPage
+import com.rainbowt0506.newsapp.presentation.onboarding.components.PageIndicator
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -32,5 +47,48 @@ fun OnBoardingScreen() {
         HorizontalPager(pageCount = pages.size, state = pageState) { index ->
             OnBoardingPage(page = pages[index])
         }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Row(
+            modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(horizontal = MediumPadding2)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PageIndicator(
+                modifier = Modifier.width(PageIndicatorWidth),
+                pageSize = pages.size,
+                selectedPage = pageState.currentPage
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+
+                val scope = rememberCoroutineScope()
+
+                if (buttonState.value[0].isNotEmpty()) {
+                    NewsTextButton(text = buttonState.value[0], onClick = {
+                        scope.launch {
+                            pageState.animateScrollToPage(page = pageState.currentPage - 1)
+                        }
+                    })
+                }
+
+                NewsButton(text = buttonState.value[1], onClick = {
+                    scope.launch {
+                        if (pageState.currentPage == 3) {
+                            //  TODO: Navigate to Home Screen
+                        } else {
+                            pageState.animateScrollToPage(
+                                page = pageState.currentPage + 1
+                            )
+                        }
+                    }
+                })
+            }
+        }
+        Spacer(modifier = Modifier.weight(0.5f))
     }
 }
