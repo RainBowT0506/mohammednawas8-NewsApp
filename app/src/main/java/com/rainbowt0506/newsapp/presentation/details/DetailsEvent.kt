f0b98554199666dd35f7477736c744cfb87cbe7f
RainBowT <rainbowt0506@gmail.com>
2024-03-06 11:27:11 +0800

    feat: Details Screen
    Implement details screen functionality
    
    - Created a composable function `DetailsTopBar` for the details screen top bar with navigation, bookmark, share, and browsing actions.
    - Implemented the `DetailsScreen` composable for displaying article details, including an image, title, and content.
    - Introduced a `DetailsEvent` sealed class with the `SaveArticle` subtype for bookmarking an article.
    - Handled actions like browsing, sharing, and bookmarking in the `DetailsScreen`.
    - Added preview functions for both `DetailsTopBar` and `DetailsScreen`.
    - Resolved an issue with the incorrect icon in the `DetailsTopBar`.

---
+++ app/src/main/java/com/rainbowt0506/newsapp/presentation/details/DetailsEvent.kt
@@ -0,0 +1,6 @@
+package com.rainbowt0506.newsapp.presentation.details
+
+sealed class DetailsEvent {
+
+    object SaveArticle : DetailsEvent()
+}
---
+++ app/src/main/java/com/rainbowt0506/newsapp/presentation/details/DetailsScreen.kt
@@ -0,0 +1,129 @@
+package com.rainbowt0506.newsapp.presentation.details
+
+import android.content.Intent
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.rainbowt0506.newsapp.R
+import com.rainbowt0506.newsapp.domain.model.Article
+import com.rainbowt0506.newsapp.domain.model.Source
+import com.rainbowt0506.newsapp.presentation.Dimens.ArticleImageHeight
+import com.rainbowt0506.newsapp.presentation.Dimens.MediumPadding1
+import com.rainbowt0506.newsapp.presentation.details.components.DetailsTopBar
+import com.rainbowt0506.newsapp.ui.theme.NewsAppTheme
+
+
+@Composable
+fun DetailsScreen(
+    article: Article,
+    event: (DetailsEvent) -> Unit,
+    navigateUp: () -> Unit
+) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+    ) {
+        DetailsTopBar(
+            onBrowsingClick = {
+                Intent(Intent.ACTION_VIEW).also {
+                    it.data = Uri.parse(article.url)
+                    if (it.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(it)
+                    }
+                }
+            },
+            onShareClick = {
+                Intent(Intent.ACTION_SEND).also {
+                    it.putExtra(Intent.EXTRA_TEXT, article.url)
+                    it.type = "text/plain"
+                    if (it.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(it)
+                    }
+                }
+            },
+            onBookMarkClick = { event(DetailsEvent.SaveArticle) },
+            onBackClick = navigateUp
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(
+                start = MediumPadding1,
+                end = MediumPadding1,
+                top = MediumPadding1
+            )
+        ) {
+            item {
+                AsyncImage(
+                    model = ImageRequest.Builder(context = context).data(article.urlToImage)
+                        .build(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(ArticleImageHeight)
+                        .clip(MaterialTheme.shapes.medium),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.height(MediumPadding1))
+
+                Text(
+                    text = article.title,
+                    style = MaterialTheme.typography.displaySmall,
+                    color = colorResource(id = R.color.text_title)
+                )
+
+                Text(
+                    text = article.content,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = colorResource(id = R.color.body)
+                )
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun DetailsScreenPreview() {
+    NewsAppTheme(dynamicColor = false) {
+        DetailsScreen(
+            article = Article(
+                author = "Wes Davis",
+                content = "Apple may not do a spring event this year...\r\nBy Wes Davis, a weekend editor who c… [+2857 chars]",
+                description = "Despite having a lot to show off this spring, including OLED iPad Pros, bigger iPad Airs, and new MacBook Airs, Apple may hold off on its spring event this year.",
+                publishedAt = "2024-03-03T14:59:21Z",
+                source = Source(id = "the-verge", name = "The Verge"),
+                title = "Apple may not do a spring event this year",
+                url = "https://www.theverge.com/2024/3/3/24089196/apple-no-spring-event-planned-macbook-air-ipad",
+                urlToImage = "https://cdn.vox-cdn.com/thumbor/H677yLcdetO3X15Cqi-RGvXVgvc=/0x0:2040x1360/1200x628/filters:focal(1020x680:1021x681)/cdn.vox-cdn.com/uploads/chorus_asset/file/23951261/VRG_Illo_N_Barclay_5_apple.jpg"
+            ),
+            event = {},
+        ) {
+
+        }
+    }
+}
